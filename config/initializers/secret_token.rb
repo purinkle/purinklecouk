283ec036dd50e4c7915a4e986276948f8f86dc09
Robert Whittaker <cyclistsdismount@gmail.com>
2012-07-08 11:03:37 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purinklecouk::Application.config.secret_token = '4a345d1efe1c24744a0b5c1e60be8babb0a2eb09807945bf10dc44a3a497b84b01f531f0942ffbd4bfaa9b289273bf6e4a9b4204ce6a591f68f1226ef40a37a5'
